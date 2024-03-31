975b532fc673d024a03adef8d765716041795b58
Mayur Shinde <24770409-MayurShinde9@users.noreply.replit.com>
2024-03-31 11:34:14 +0000

    add p tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,15 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 import "./App.css";
 
 export default function App() {
   let [displayName, setDisplayName] = useState(false);
   let [fullname, setFullname] = useState("");
 
-  let name = useRef("");
-  let lastName = useRef("");
-
-  const checkSpecialCharacter = (value) => {
-    let specialCharacters = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
-    if (value.match(specialCharacters)) {
-      return true;
-    } else {
-      return false;
-    }
-  };
+  let [name, setName] = useState("");
+  let [lastName, setLastName] = useState("");
 
   const validate = (nameT, lastNameT) => {
     if (nameT.trim() === "" || lastNameT.trim() === "") {
-      return false;
-    }
-
-    if (checkSpecialCharacter(nameT) || checkSpecialCharacter(lastNameT)) {
       return false;
     }
     return true;
@@ -31,31 +18,42 @@ export default function App() {
   const handleFormSubmit = (e) => {
     e.preventDefault();
     setDisplayName(true);
-    let nameT = name.current.value;
-    let lastNameT = lastName.current.value;
-
-    if (!validate(nameT, lastNameT)) {
+    if (!validate(name, lastName)) {
       return;
     }
-    setFullname(`Full Name: ${nameT} ${lastNameT}`);
+    setFullname(`Full Name: ${name} ${lastName}`);
   };
 
   return (
     <main>
       <h1>Full Name Display</h1>
       <form>
-      <label>First Name:</label>
-      <input type="text" id="fname" name="fname" ref={name} value={name.current.value} required/>
-      <br />
-      <label>Last Name:</label>
-      <input type="text" id="fname" name="lname" ref={lastName} value={lastName.current.value} required />
-      <br />
-      <button type="submit" onClick={handleFormSubmit}>
-        Submit
-      </button>
-      <br />
+        <label>First Name:</label>
+        <input
+          type="text"
+          id="fname"
+          name="fname"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          required
+        />
+        <br />
+        <label>Last Name:</label>
+        <input
+          type="text"
+          id="fname"
+          name="lname"
+          onChange={(e) => setLastName(e.target.value)}
+          value={lastName}
+          required
+        />
+        <br />
+        <button type="submit" onClick={handleFormSubmit}>
+          Submit
+        </button>
+        <br />
       </form>
-      <p>{displayName && {fullname}}</p>
+      {displayName && <p>{fullname}</p>}
     </main>
   );
 }
